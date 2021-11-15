4d8859c459aa955173e11ec1b57f8077708f7d60
soumik2345 <soumiksaha2345@gmail.com>
2021-11-15 16:26:20 +0600

    geolocation app to get user location and host in heroku

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import "./App.css";
 
-function App() {
+const App = () => {
+
+  const [location, setLocation] = useState("");
+
+  const apiKey = process.env.REACT_APP_HERE_API_KEY;
+
+  const getLocation = () =>{
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition,showError);
+    } else {
+      setLocation("Geolocation is not supported by this browser.")
+    }
+  }
+
+const showPosition = async (position) =>{
+  const latitude = await position.coords.latitude ;
+  const longitude = await position.coords.longitude;
+
+  const url = `https://revgeocode.search.hereapi.com/v1/revgeocode?at=${latitude}%2C${longitude}&lang=en-US&apikey=${apiKey}`
+
+  const location = await fetch(url)
+
+  const data = await location.json()
+
+  const city = data.items[0].address.city;
+  setLocation(city);
+}
+
+
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      setLocation( "User denied the request for Geolocation.")
+      break;
+    case error.POSITION_UNAVAILABLE:
+      setLocation("Location information is unavailable.")
+      break;
+    case error.TIMEOUT:
+      setLocation("The request to get user location timed out.")
+      break;
+    case error.UNKNOWN_ERROR:
+      setLocation( "An unknown error occurred.")
+      break;
+    default :
+      setLocation( "An unknown error occurred.")
+      break;
+  }
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      
+
+<div className="header">
+
+    <button className="btn" onClick={ (e)=>getLocation() } >{ location ? "City : " + location : "Detect your location"}</button>
+
+</div>
+
+
+    </>
+  )
 }
 
 export default App;
